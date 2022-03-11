93909b516759c1dea8cc34dd9547eca515f7c850
Richard <iwannaplaygamesjp@gmail.com>
2022-03-11 20:16:29 +0800

    first commit. init app and server

--- src/types/events.ts
+++
@@ -1,8 +0,0 @@
-export enum EventKeys
-{
-    OnceStateChanged = 'once-state-changed',
-    BoardChanged = 'board-changed',
-    PlayerTurnChanged = 'player-turn-changed',
-    PlayerWin = 'player-win',
-    GameStateChanged = 'game-state-changed'
-}
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @type {import('vite').UserConfig}
- */
-const config = {
-// ...
-}
-
-export default config
