5016b16b11180512ccacc353cbe8b012b6ce842a
Richard <iwannaplaygamesjp@gmail.com>
2022-03-12 21:03:58 +0800

    using ourcade parcel ts template instead

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,55 +1,18 @@
-import './style.css'
+import Phaser from 'phaser'
 
-// const app = document.querySelector<HTMLDivElement>('#app')!
+import HelloWorldScene from './scenes/HelloWorldScene'
 
-// app.innerHTML = `
-//   <h1>Hello Vite!</h1>
-//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-//   <div id="game"></div>
-// `;
-
-import 'phaser';
-import { MenuScene } from './menu-scene';
-
-const GameConfig: Phaser.Types.Core.GameConfig = {
-  title: 'ExampleGame',
-  url: 'https://github.com/digitsensitive/phaser3-typescript',
-  version: '2.0',
-  width: 800,
-  height: 600,
-  type: Phaser.AUTO,
-  parent: 'app',
-  scene: [MenuScene],
-  input: {
-    keyboard: true
-  },
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 0 },
-      debug: false
-    }
-  },
-  backgroundColor: '#300000',
-  render: { pixelArt: false, antialias: true },
-  scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    // `fullscreenTarget` must be defined for phones to not have
-    // a small margin during fullscreen.
-    fullscreenTarget: 'app',
-    expandParent: false,
-  },
-};
-
-
-export class Game extends Phaser.Game {
-  constructor(config: Phaser.Types.Core.GameConfig) {
-    super(config);
-  }
+const config: Phaser.Types.Core.GameConfig = {
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 }
+		}
+	},
+	scene: [HelloWorldScene]
 }
 
-window.addEventListener('load', () => {
-  // Expose `_game` to allow debugging, mute button and fullscreen button
-  (window as any)._game = new Game(GameConfig);
-});
+export default new Phaser.Game(config)
