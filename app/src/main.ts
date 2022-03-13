a66ff4a01a1f44e560ac188644474184581d19a0
Richard <iwannaplaygamesjp@gmail.com>
2022-03-13 11:29:21 +0800

    added vite app

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,18 +1,23 @@
 import Phaser from 'phaser'
 
 import HelloWorldScene from './scenes/HelloWorldScene'
+import LoadScene from './scenes/Load'
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: window.innerHeight * 1.33,
+        height: window.innerHeight
+    },
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 200 }
+			gravity: { y: 0 }
 		}
 	},
-	scene: [HelloWorldScene]
+	scene: [HelloWorldScene, LoadScene]
 }
 
 export default new Phaser.Game(config)
--- app/src/scenes/Load.ts
+++ app/src/scenes/Load.ts
@@ -1,39 +1,19 @@
 import Phaser from 'phaser'
 
-export default class HelloWorldScene extends Phaser.Scene
+export default class LoadScene extends Phaser.Scene
 {
 	constructor()
 	{
-		super('hello-world')
+		super('load')
 	}
 
 	preload()
     {
-        this.load.setBaseURL('http://labs.phaser.io')
-
-        this.load.image('sky', 'assets/skies/space3.png')
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png')
-        this.load.image('red', 'assets/particles/red.png')
+        this.load.atlas('all-cards', '../assets/all-cards.png', '../assets/all-cards.json')
     }
 
     create()
     {
-        this.add.image(400, 300, 'sky')
-
-        const particles = this.add.particles('red')
-
-        const emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        })
-
-        const logo = this.physics.add.image(400, 100, 'logo')
-
-        logo.setVelocity(100, 200)
-        logo.setBounce(1, 1)
-        logo.setCollideWorldBounds(true)
-
-        emitter.startFollow(logo)
+        this.scene.start("game")
     }
 }
